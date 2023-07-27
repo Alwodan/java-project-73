d3cc008d9c3c561f85dd557b32f010faa2c859c4
Vladislav <mistergeralt@gmail.com>
2023-07-28 02:52:01 +0300

    worst stage in my life

---
+++ app/src/main/java/hexlet/code/app/model/User.java
@@ -0,0 +1,39 @@
+package hexlet.code.app.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Timestamp createdAt;
+
+}
