c3bd7eda339a37abd2b53740c406b8e518e39004
Vladislav <mistergeralt@gmail.com>
2023-08-13 13:09:36 +0300

    test

--- app/src/main/java/hexlet/code/app/AppApplication.java
+++ app/src/main/java/hexlet/code/app/AppApplication.java
@@ -1,16 +1,11 @@
 package hexlet.code.app;
 
-import com.rollbar.notifier.Rollbar;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppApplication {
-    @Autowired
-    static Rollbar rollbar;
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
-        rollbar.debug("Here is some debug message");
     }
 }
